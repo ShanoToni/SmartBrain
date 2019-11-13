e225d8049065925f2b1baa8f95996ad47612b755
Anton <anton_b_mitkov@abv.bg>
2019-11-13 23:45:54 +0000

    Deploy of SmartBrain

---
+++ precache-manifest.cd5e11e7b5b6b8ef8711bcde49d40341.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178713a17fc199d0189384404df1b1a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3e5386c76d6e701d743",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a3b81b027a39db9dc4c7",
+    "url": "/static/css/main.ce37e9c9.chunk.css"
+  },
+  {
+    "revision": "f3e5386c76d6e701d743",
+    "url": "/static/js/2.49983f8c.chunk.js"
+  },
+  {
+    "revision": "a3b81b027a39db9dc4c7",
+    "url": "/static/js/main.fb16770f.chunk.js"
+  },
+  {
+    "revision": "1aad1282a2547bf5d9ea",
+    "url": "/static/js/runtime-main.00161c69.js"
+  },
+  {
+    "revision": "7cfc8fbe990adb571447ecc35a06d841",
+    "url": "/static/media/brain.7cfc8fbe.png"
+  }
+]);
